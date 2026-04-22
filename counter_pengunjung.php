95b4b4132893e1014e67c3c01e27267538731970
prayk04 <prayukiyat@gmail.com>
2026-04-22 13:21:17 +0700

    after

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -6,13 +6,24 @@ class Pengunjung {
     public function __construct() {
         self::$jumlah++;
     }
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah . "<br>";
+echo "Jumlah Pengunjung (Sebelum Reset): " . Pengunjung::$jumlah . "<br>";
+
+// 3. Memanggil method static reset
+Pengunjung::reset();
+
+// 4. Menampilkan hasil SESUDAH di reset
+echo "Jumlah Pengunjung (Setelah Reset): " . Pengunjung::$jumlah;
 
 
 ?>
